3da4b1e3b1244bbea47fcad9665da07140b7ddce
Ubuntu <ubuntu@ip-172-31-46-83.ec2.internal>
2020-03-31 19:12:53 +0000

    commit message

---
+++ ICS3UUnit1O4.java
@@ -0,0 +1,16 @@
+// Copyright (c) Ac industries All rights reserved  
+//
+// Created by: Andrew Coxall
+// Created on: Mar 31, 2020
+// This is the area of a square with a length of 5 and a hight of 3 (cm)
+
+public class ICS3UUnit1O4 {
+	public static void main(String[] args) {
+
+    	int area = 5*3;
+    	int perimeter = 5*2 + 3*2;
+    	
+    	System.out.println("Area is " + area);
+    	System.out.println("Perimeter is " + perimeter);
+	}
+}
